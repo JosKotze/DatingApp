9632bde840260210deddc52447e29e6d48fad8db
Jos <jpkotze148@gmail.com>
2024-08-22 20:48:18 +0200

    Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -13,11 +13,15 @@ builder.Services.AddDbContext<DataContext>(opt =>
     // get section connection string.
 });
 
+builder.Services.AddCors();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
