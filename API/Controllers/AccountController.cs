89804192a06269a5a4cf9a4fcbe2328ab49bcb82
Jos <jpkotze148@gmail.com>
2024-10-16 20:55:24 +0200

    Section 12

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -5,13 +5,14 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class AccountController(DataContext context, ITokenService tokenService, IPhotoService photoService) : BaseApiController
+public class AccountController(DataContext context, ITokenService tokenService, IPhotoService photoService, IMapper mapper) : BaseApiController
 {
     [HttpPost("register")]
     public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto){
@@ -20,23 +21,23 @@ public class AccountController(DataContext context, ITokenService tokenService, 
         {
             return BadRequest("A user with that name is already in the system");
         }
-        return Ok();
 
-        // using var hmac = new HMACSHA512(); // using 'using' to dispose it after use
+        using var hmac = new HMACSHA512(); // using 'using' to dispose it after use
 
-        // var user = new AppUser{
-        //     UserName = registerDto.Username.ToLower(),
-        //     PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-        //     PasswordSalt = hmac.Key
-        // };
+        var user = mapper.Map<AppUser>(registerDto);
 
-        // context.Users.Add(user);
-        // await context.SaveChangesAsync();
+        user.UserName = registerDto.Username.ToLower();
+        user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
+        user.PasswordSalt = hmac.Key;
 
-        // return new UserDto{
-        //     UserName = user.UserName,
-        //     Token = tokenService.CreateToken(user)
-        // };
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        return new UserDto{
+            UserName = user.UserName,
+            Token = tokenService.CreateToken(user),
+            KnownAs = user.KnownAs
+        };
     }
 
     [HttpPost("login")]
@@ -59,6 +60,7 @@ public class AccountController(DataContext context, ITokenService tokenService, 
 
         return new UserDto{
             UserName = user.UserName,
+            KnownAs = user.KnownAs,
             Token = tokenService.CreateToken(user),
             PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
         };
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,34 +1,72 @@
-import { Component, EventEmitter, inject, input, output, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, OnInit, output } from '@angular/core';
 import { AccountService } from '../services/account.service';
-import { ToastrService } from 'ngx-toastr';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+import { JsonPipe, NgIf } from '@angular/common';
+import { TextInputComponent } from "../forms/text-input/text-input.component";
+import { DatePickerComponent } from '../forms/date-picker/date-picker.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, JsonPipe, NgIf, TextInputComponent, DatePickerComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   cancelRegister = output<boolean>();
-  model: any = {}
   private accountService = inject(AccountService);
-  private toastr = inject(ToastrService);
+  private fb = inject(FormBuilder);
+  registerForm: FormGroup = new FormGroup({});
+  maxDate = new Date();
+  router = inject(Router);
+  validationErrors: string[] | undefined;
+
+  ngOnInit(): void {
+    this.initializeForm();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() -18)
+  }
+
+  initializeForm() {
+    this.registerForm = this.fb.group({
+      gender: ['male'],
+      username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', [Validators.required, this.matchValues('password')]]
+    });
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+    })
+  }
+  
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value == control.parent?.get(matchTo)?.value ? null : {isMatching: true}
+    }
+  }
 
   register() {
-    this.accountService.register(this.model).subscribe({
-      next: response => {
-        console.log(response);
-        this.cancel();
-      },
-      error: err => {
-        this.toastr.error(err.error)
-      }
+    // const dob = this.getDateOnly(this.registerForm.get('sateOfBirth')?.value);
+    // this.registerForm.patchValue({dateOfBirth: dob});
+    const dob = this.getDateOnly(this.registerForm.get('dateOfBirth')?.value);
+    this.registerForm.value.dateOfBirth = dob;
+    this.accountService.register(this.registerForm.value).subscribe({
+      next: _ => this.router.navigateByUrl('/member'),
+      error: err => this.validationErrors = err
     })
   }
 
   cancel() {
     this.cancelRegister.emit(false);
+  }
+
+  private getDateOnly(dob: string | undefined){
+    if (!dob) return;
+    return new Date(dob).toISOString().slice(0, 10);
   }
 }
